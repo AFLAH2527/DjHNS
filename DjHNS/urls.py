9e13bab50f9c552b9e2aa3e40a7090179106eb2d
Rifana Sherin <rifanasherin44@gmail.com>
2022-03-07 23:22:41 -0800

    Front page change

--- DjHNS/urls.py
+++ DjHNS/urls.py
@@ -24,7 +24,7 @@ urlpatterns = [
     path('home/', home_page, name='home'),
     path('stock_management/', stock_management, name='stock-management'),
     path('register', user_view.register, name='user-register'),
-    path('', auth_views.LoginView.as_view(template_name='user/login.html'), name='user-login'),
+    path('login/', auth_views.LoginView.as_view(template_name='user/login.html'), name='user-login'),
     path('logout/', auth_views.LogoutView.as_view(template_name='user/logout.html'), name='user-logout'),
     path('vaccine/', include(('vaccine.urls', 'vaccine'), namespace= "vaccine")),
     path('bloodbank/', include(('bloodbank.urls', 'bloodbank'), namespace = 'bloodbank')),
